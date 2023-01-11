aa998b962f567a137f4472f5bf75ee8d97e20559
Your Jonathan Paramo <jhonathan-and@outlook.com>
2023-01-10 19:57:10 -0500

    Cambio la manera en que se realiza la petion falta revision

--- src/RickDashboard.jsx
+++ src/RickDashboard.jsx
@@ -11,39 +11,69 @@ export const RickDashboard = () => {
   const {
     location = [],
     isLoading,
-    residents: resident = [],
+    resident = [],
   } = useSelector( state => state.rickAndMorty )
 
-  useEffect(() => {
-    dispatch( getLocation() );
-  }, [])
+  //  const infoPj = () => {
+  //   // const pj = callCaracter()
+  //   const regex = /(\d+)/g;
+  //   // let url = pj.filter(urls => urls != undefined)
+
+  //   Promise.all(url.map( element => {
+  //     return rickApiCaracter.get("/"+element.match(regex))
+  //   }))
+  //   .then(res => {
+  //     const inf = []
+
+  //     res.forEach(({data}) => {
+  //       inf.push(data)
+  //       setDataPj(inf)
+  //     })
+  //   });
+  // };
 
   const callCaracter = () => {
     const residente = location.map(({ residents }) => residents)
-    const peti = residente.map( resi => resi[0])
-    return peti
+
+    residente.map( resi => {
+      const regex = /(\d+)/g;
+
+      if (resi[0] !== undefined) {
+        const peti = rickApiCaracter.get("/"+resi[0].match(regex))
+        .then(({data}) => {
+          // return setDataPj((preState) => [...preState, data])
+          return data
+        })
+        .then(data => setDataPj((prevState) => [... prevState, data]))
+        return peti
+      }
+      // let url = resi.filter(urls => urls !== undefined )
+      //   console.log(url, "urls")
+    })
   }
 
-  const infoPj = () => {
-    const pj = callCaracter()
-    const regex = /(\d+)/g;
-    let url = pj.filter(urls => urls != undefined)
+  useEffect(() => {
+    dispatch( getLocation() );
+    callCaracter()
+  }, [])
 
-    Promise.all(url.map( element => { return rickApiCaracter.get("/"+element.match(regex))}))
-    .then(res => {
-      const inf = []
+  // const infoPj = () => {
+  //   const pj = callCaracter()
+  //   const regex = /(\d+)/g;
+  //   let url = pj.filter(urls => urls != undefined)
 
-      res.forEach(({data}) => {
-        // aqui tengo la data, pero al momento de querer guardarla en el state se crea un bucle,
-        // o si la retorno no sale del .then
-      });
-      inf
-    });
-  };
+  //   Promise.all(url.map( element => { return rickApiCaracter.get("/"+element.match(regex))}))
+  //   .then(res => {
+  //     const inf = []
 
-  infoPj()
-    // setData((prevState) => [...prevState, response]);
-    // const response = peticion(residents[0]);
+  //     res.forEach(({data}) => {
+  //       // aqui tengo la data, pero al momento de querer guardarla en el state se crea un bucle,
+  //       // o si la retorno no sale del .then
+  //     });
+  //     inf
+  //   });
+  // };
+
   const hasData = location?.length > 0;
 
   return (
@@ -70,18 +100,22 @@ export const RickDashboard = () => {
           type,
           residents,
         }) => {
-          // const image = data.filter(({ id: personajeId }) => id === personajeId )
+
           return (
             <section key={id}>
-            <p>{id}</p>
-            <h3>dimension: {dimension}</h3>
-            <p>Name: {name}</p>
-            <p>Type: {type}</p>
-            <footer>Created: {created}</footer>
-            {
-
-            }
-          </ section>
+              <p>{id}</p>
+              <h3>dimension: {dimension}</h3>
+              <p>Name: {name}</p>
+              <p>Type: {type}</p>
+              <footer>Created: {created}</footer>
+              {
+                // dataPj.map(({ id, image }) => {
+                //   // if (arr.includes(id) && id === id) {
+                //     return <img className="image-pj" key={id} src={image}/>
+                //   // }
+                // })
+              }
+            </section>
           )
         })
       }
