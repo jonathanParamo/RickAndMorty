e4f4a60cbaa81e2a4c16c4189de069a192e15aaa
Your Jonathan Paramo <jhonathan-and@outlook.com>
2023-01-11 18:09:06 -0500

    termino el diseño de cada card de la dimencion y creo codigo para acceder a las imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { store } from './store'
 import { Provider } from 'react-redux'
-import { RickDashboard } from './RickDashboard'
+import { RickDashboard } from './components/RickAndMortyDashboard/RickDashboard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
